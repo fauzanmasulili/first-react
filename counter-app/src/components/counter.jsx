4b4adb615ccbdd432cce05291652f9f6eb37d2b5
FauzanKhalifMasulili <fauzan.masulili@gmail.com>
2018-10-19 19:54:40 +0700

    counters

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,31 +2,38 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1','tag2','tag3']
-      }
+        value: this.props.counter.value
+      };
+
+    // constructor(){
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement = () => {
+        this.setState({value: this.state.value + 1});
+    }
+
     render() { 
        return ( 
         <div>
             <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-            </ul>
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
         );
        }
-
+       /* Method to get Badge Classes dynamicly */
        getBadgeClasses(){
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes
        }
 
-
+       /* Method to check count value */
        formatCounter (){
-           const {count} = this.state;
-           return count === 0 ? "Zero" : count;
+           const {value} = this.state;
+           return value === 0 ? "Zero" : value;
        }
     }
 
